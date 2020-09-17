4cfc3eef20d2cedc1f7e47d0d1e0f9344fedb4a1
danielteodoro <daniel.missimat@gmail.com>
2020-09-16 22:42:09 -0300

    fix optional values

--- SimpleNews/View Models/ArticleViewModel.swift
+++ SimpleNews/View Models/ArticleViewModel.swift
@@ -42,10 +42,10 @@ extension ArticleViewModel {
 
 extension ArticleViewModel {
     var title: String {
-        return self.article.title
+        return self.article.title ?? ""
     }
     
     var description: String {
-        return self.article.description
+        return self.article.description ?? ""
     }
 }
